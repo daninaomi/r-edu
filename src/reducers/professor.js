0f48b5a0633d469a92553224593b8300ad3c3811
unknown <leonardocouceiro.sp@gmail.com>
2018-04-05 21:51:55 -0300

    continuando acerto para receber dados no perfil professor

---
+++ src/reducers/professor.js
@@ -0,0 +1,19 @@
+import { PEGA_PERFIL_PROFESSOR } from "../actions";
+
+
+export function professor(estadoAtual = {}, acao) {
+    switch (acao.type) {
+        case PEGA_PERFIL_PROFESSOR:
+            let novoEstado = {};
+
+            
+                novoEstado[acao.professor.idProfessor] = acao.professor
+    
+
+            return novoEstado
+        default:
+            return estadoAtual
+    }
+}
+
+
